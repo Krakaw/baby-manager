7d08b3b5ac66eb97adee7232cbe7270cd48e39ab
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-06-19 19:49:40 +0200

    Cleaning up file

--- server/models/playbooks/index.js
+++ server/models/playbooks/index.js
@@ -1,12 +1,8 @@
 const Playbooks = require('./playbooks')
-const Item = require('./item')
-
 const playbooks = new Playbooks(process.env.CONFIG_FILE)
 if (process.env.AUTO_START_STREAMS) {
-  playbooks.playbooks.forEach(playbook => {
-    playbook.items.filter(item => item.type === Item.TYPES.TYPE_RTSP_STREAM).forEach(item => {
-      item.run()
-    })
+  playbooks.streams.forEach(item => {
+    item.run()
   })
 }
 module.exports = playbooks
