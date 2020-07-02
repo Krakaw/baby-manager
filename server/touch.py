72c0133052453c7c2ba7a5d0886a7ac160e1ff3a
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-07-02 10:58:12 +0200

    Add status light

--- server/touch.py
+++ server/touch.py
@@ -16,23 +16,30 @@ for pad in ['Back','A','B','C','D','Enter']:
     touchphat.set_led(pad, False)
     time.sleep(0.1)
 
+touchphat.set_led('A', True)
 
 @touchphat.on_touch(['A'])
 def handle_start(event):
-    r = requests.post('http://localhost:3000/playbooks/start', json={'name': 'Bedtime'}, timeout=2)
-    print(r)
-    if r.status_code == 200:
-        flash(['A'])
-    else:
+    try:
+        r = requests.post('http://localhost:3000/playbooks/start', json={'name': 'Bedtime'}, timeout=2)
+        print(r)
+        if r.status_code == 200:
+            flash(['A'])
+        else:
+            flash(['Back','A','B','C','D','Enter'])
+    except:
         flash(['Back','A','B','C','D','Enter'])
 
 @touchphat.on_touch(['D'])
 def handle_stop(event):
-    r = requests.post('http://localhost:3000/playbooks/stop', json={'name': 'Bedtime'}, timeout=2)
-    print(r)
-    if r.status_code == 200:
-        flash(['D'])
-    else:
+    try:
+        r = requests.post('http://localhost:3000/playbooks/stop', json={'name': 'Bedtime'}, timeout=2)
+        if r.status_code == 200:
+            flash(['D'])
+            touchphat.set_led('A', True)
+        else:
+            flash(['Back','A','B','C','D','Enter'])
+    except:
         flash(['Back','A','B','C','D','Enter'])
 
 def flash(pads):
