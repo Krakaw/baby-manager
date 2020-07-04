6af16392f43d6ac27e1f823ff94c227fdd16a448
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-07-04 09:04:17 +0200

    Add a night mode

--- server/touch.py
+++ server/touch.py
@@ -2,45 +2,31 @@ import signal
 import time
 import requests
 import touchphat
+import datetime
 
-print("""
-Baby Goes What Touch Started...
-Press A to start Bedtime routine
-Press B to end Bedtime routine
-Press Ctrl+C to exit!
-""")
-
-for pad in ['Back','A','B','C','D','Enter']:
-    touchphat.set_led(pad, True)
-    time.sleep(0.1)
-    touchphat.set_led(pad, False)
-    time.sleep(0.1)
-
-touchphat.set_led('A', True)
 
 @touchphat.on_touch(['A'])
 def handle_start(event):
-    try:
-        r = requests.post('http://localhost:3000/playbooks/start', json={'name': 'Bedtime'}, timeout=2)
-        print(r)
-        if r.status_code == 200:
-            flash(['A'])
-        else:
-            flash(['Back','A','B','C','D','Enter'])
-    except:
-        flash(['Back','A','B','C','D','Enter'])
+    send_request('start', 'Bedtime', 'A')
+
 
 @touchphat.on_touch(['D'])
 def handle_stop(event):
+    send_request('stop', 'Bedtime', 'D')
+
+
+def send_request(endpoint, name, pad):
     try:
-        r = requests.post('http://localhost:3000/playbooks/stop', json={'name': 'Bedtime'}, timeout=2)
+        r = requests.post('http://localhost:3000/playbooks/' + endpoint, json={'name': name}, timeout=2)
         if r.status_code == 200:
-            flash(['D'])
+            flash([pad])
             touchphat.set_led('A', True)
         else:
-            flash(['Back','A','B','C','D','Enter'])
+            flash(['Back', 'A', 'B', 'C', 'D', 'Enter'])
     except:
-        flash(['Back','A','B','C','D','Enter'])
+        flash(['Back', 'A', 'B', 'C', 'D', 'Enter'])
+    night_off()
+
 
 def flash(pads):
     for i in range(10):
@@ -51,5 +37,30 @@ def flash(pads):
             touchphat.set_led(pad, False)
         time.sleep(0.1)
 
+
+def is_night():
+    now = datetime.datetime.now()
+    today6pm = now.replace(hour=18, minute=0, second=0, microsecond=0)
+    today7am = now.replace(hour=7, minute=0, second=0, microsecond=0)
+    return now > today6pm or now < today7am
+
+
+def night_off():
+    if is_night():
+        touchphat.all_off()
+
+
+print("""
+Baby Goes What Touch Started...
+Press A to start Bedtime routine
+Press D to end Bedtime routine
+Press Ctrl+C to exit!
+""")
+
+if not is_night():
+    flash(['Back', 'A', 'B', 'C', 'D', 'Enter'])
+    touchphat.set_led('A', True)
+
+night_off()
 
 signal.pause()
