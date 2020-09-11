f4a9f2ef721f455e17d2cdb6ddf43c192805b9f2
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-09-11 04:00:00 +0200

    Fix stopper for process exit

--- server/helpers/shell-command.js
+++ server/helpers/shell-command.js
@@ -1,9 +1,15 @@
 const { exec, spawn } = require('child_process')
+
 class ShellCommand {
   launch (item, next, addStopper) {
     const process = spawn(item.params.command, item.params.opts.args)
     const pid = process.pid
     console.log('Spawned: ', pid, '[', item.params.command, item.params.opts.args, ']')
+    process.on('exit', (code) => {
+      addStopper(() => {
+        console.log('Process already exited with code: ', code)
+      })
+    })
     addStopper(() => {
       if (item.params.opts.stopCommand) {
         exec(item.params.opts.stopCommand)
